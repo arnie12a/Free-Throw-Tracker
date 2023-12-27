de8c5f4caa8b81c8ce74383320f183c9b26530e6
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2023-12-26 18:12:02 -0600

    have the database connection tot he application and can add ft session data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,11 +11,39 @@ def get_db_connection():
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/freethrowlog')
+def freethrowlog():
     conn = get_db_connection()
     logs = conn.execute('SELECT * FROM freethrowlog').fetchall()
-    print(logs)
     conn.close()
-    return render_template('index.html', logs=logs)
+    return render_template('log.html', logs=logs)
+
+@app.route('/statistics')
+def statistics():
+
+    return render_template('stats.html')
+
+@app.route('/add', methods = ["GET", "POST"])
+def add():
+    if request.method == "POST":
+
+        date = str(request.form.get("date"))
+        ftmade = request.form.get("ftmade")
+        ftattempted = request.form.get("ftattempted")
+
+        # Do some validation
+        if date and ftmade and ftattempted:
+            if ftattempted > ftmade:
+                conn = get_db_connection()
+                conn.execute("INSERT INTO freethrowlog (sessionDate, ftMade, ftAttempted) VALUES (?, ?, ?)",
+                    (date, ftmade, ftattempted)
+                    )
+                conn.commit()
+                conn.close()
+    return render_template('add.html')
+
 
 if __name__ == '__main__':  
    app.run(debug=True)
